7667b6a824fb3a748dc56ef10b54823824690e65
Dmytro Sokolov <xcdxster@gmail.com>
2025-04-04 14:46:06 +0200

    Fix base url for the project

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Visual-Editor/',
+  base: '/Visual-Editor',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
